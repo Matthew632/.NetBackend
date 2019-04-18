b98573c69ba3c569f11dda3492a7336162ad3520
Matthew <Matthew@LAPTOP-80PK4CQF>
2019-04-18 19:27:09 +0100

    finishes get all restaurants endpoint

--- MattFinalProject/Repos/ResTwoRepo.cs
+++ MattFinalProject/Repos/ResTwoRepo.cs
@@ -11,22 +11,23 @@ namespace MattFinalProject.Repos
         {
             string conString = "Host=localhost;username=postgres;password=password;Database=final_project";
             ResSummary res = null;
-            ResList allRes = null;
+            ResList allRes = new ResList();
+            int counter = 0;
             using (var con = new NpgsqlConnection(conString))
             {
                 con.Open();
                 using (var cmd = new NpgsqlCommand($"SELECT * FROM restaurants", con))
                 using (var reader = cmd.ExecuteReader())
                     while (reader.Read())
+                    {
                         res = new ResSummary { restaurant_id = reader.GetInt32(0), restaurant_name = reader.GetString(1), restaurant_address = reader.GetString(2) };
-                if (allRes == null)
-                {
-                    allRes = new ResList();
-                }
-                allRes.resList.Add(res);
+                        allRes.resList.Insert(counter, res);
+                        counter++;
+                    }
+                    
+            }
                 return allRes;
 
             }
         }
     }
-}
